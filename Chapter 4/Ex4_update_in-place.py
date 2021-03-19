7f8f277a48f3d46a04c45f37d4bba3af0766fb95
Arnik9 <impwithafurrytail@gmail.com>
2021-03-19 13:07:32 +0300

    Update and rename Ex4.2.py to Ex4_update_in-place.py

--- Chapter 4/Ex4_update_in-place.py
+++ Chapter 4/Ex4_update_in-place.py
@@ -3,21 +3,21 @@ from tabulate import tabulate
 
 class State: # class for each unit on the game
     def __init__(self, _id):
-        self.value = 0
+        if _id != 0:
+            self.value = 0.0
+        elif _id == 0:
+            self.value = 0.0
         self.id = _id
         self.left_bound = max(1, (self.id // 4) * 4)  # save the left border
         self.right_bound =  min(14, (self.id // 4) * 4 + 3) # save the right border
-        self.nextS = [self.move('L'), self.move('R'), self.move('U'), self.move('D')]
 
 
-    def move(self, u):  # get next S
+def move(self, u):  # get next S
         if u == 'L':  # move left
             if self.id - 1 >= self.left_bound:
                 return self.id - 1
             elif self.id - 1 == 0:
                 return 0
-            elif self.id == 16:
-                return 16
             else:
                 return self.id
         if u == 'R':  # move right
@@ -25,8 +25,6 @@ class State: # class for each unit on the game
                 return self.id + 1
             elif self.id + 1 == 15:
                 return 0
-            elif self.id == 16:
-                return 16
             else:
                 return self.id
         if u == 'U':  # move up
@@ -34,8 +32,6 @@ class State: # class for each unit on the game
                 return self.id - 4
             elif self.id - 4 == 0:
                 return 0
-            elif self.id == 16:
-                return 13
             else:
                 return self.id
         if u == 'D':  # move down
@@ -43,58 +39,46 @@ class State: # class for each unit on the game
                 return self.id + 4
             elif self.id + 4 == 15:
                 return 0
-            elif self.id == 13:
-                return 16
             else:
                 return self.id
 
-    def update(self, S):  # S is the whole set of the States.
+def update(self, S): 
         V = 0
-        for i in range(0, 4):
-            V += S[self.nextS[i]].value
-        # print("update id " + str(self.id))
-        # print("for the " + str(self.nextS[i]))
-        # print("old value" + str(self.value))
-        # print("new value" + str(S[self.nextS[i]].value))
-        self.value = -1+0.25 * V
+        V = -4 + S[move(self, 'U')].value + S[move(self, 'D')].value + S[move(self, 'R')].value + S[move(self, 'L')].value
+        #print("update id " + str(self.id) + "old value" + str(self.value) + "new value" + str(Old[move(self, 'U')]))
+        self.value = 0.25 * V
 
 def train(k=10):
-    V  = []
+    V  = [0 for i in range(0, 16)]
     S_T = State(0)
     S = {0: S_T}
+
     for j in range(1, 15):
         S[j] = State(j)
-    S[16] = State(16)
-    for loop in range(k):
-        if loop>=1000 and loop%1000 == 0:
-            print("Training "+str(loop)+"'s loop.......Remaining: "+str(k-loop)+ " loops")
-        n = numpy.random.random()
-        if n > 0.5:
-            for j in range(1, 15):
-                S[j].update(S)
-        else:
-            for j in range(14, 0, -1):
-                S[j].update(S)
-        S[16].update(S)
-    for t in range(0,17):
-        if t == 0 or t == 15:
-            V.append("0")
-        else:
-            V.append(S[t].value)
-    draw(V)
 
+    for loop in range(k):
+      
+      for j in range(1, 15):
+        update(S[j], S)
+        
+      for t in range(0,16):
+        if t == 0 or t == 15:
+          V[t] = 0
+        else:
+          V[t] = S[t].value
+
+    draw(V)
 
 def draw(valueArray):
     for i in range(4):
         print("----------------------")
-        print("| "+str(int(valueArray[i*4]))+" | "+str(int(valueArray[i*4+1])) +" | "+str(int(valueArray[i*4+2])) +" | "+str(int(valueArray[i*4+3])) +" |")
-    print("----------------------")
-    print("|    | "+str(int(valueArray[16])) +" |    |   |")
+        print("| "+str(round((valueArray[i*4]),1))+" | "+str(round((valueArray[i*4+1]),1)) +" | "+str(round((valueArray[i*4+2]),1)) +" | "+str(round((valueArray[i*4+3]),1)) +" |")
     print("----------------------")
     print("Accurate State Values List:")
     for i in range(1,8):
         print("State "+str(2*i-1)+": "+str(valueArray[2*i-1])+ "          State "+str(2*i)+": "+str(valueArray[2*i]))
-
+        
+        
 if __name__ == '__main__':
     k = input("Specify the desired training loop count(0-10000):")
     train(int(k))
